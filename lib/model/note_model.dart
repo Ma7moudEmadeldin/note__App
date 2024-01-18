bab08f9a1e2532167559f03bbf07c2fa66cf2e6e
mahmoud emadeldin <mahmoudemad21497@gmail.com>
2024-01-18 22:32:54 +0200

    init hive

---
+++ lib/model/note_model.dart
@@ -0,0 +1,15 @@
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class note_Model {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subtitle;
+  @HiveField(2)
+  final String date;
+
+  note_Model({required this.title, required this.subtitle, required this.date});
+}
