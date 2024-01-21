a58efcfe6b30262409cc46c365d67eacb104619c
mahmoud emadeldin <mahmoudemad21497@gmail.com>
2024-01-21 11:02:19 +0200

    format date

--- lib/views/addnote_Items.dart
+++ lib/views/addnote_Items.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:noteapp/cubit/addNoteCubit/addNotecubit.dart';
 import 'package:noteapp/model/note_Model.dart';
 import 'package:noteapp/views/addButton.dart';
@@ -51,10 +52,12 @@ class _addNoteFormState extends State<addNoteForm> {
                 onTap: () {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
+                    var currentDate = DateTime.now();
+                    var formateDate = DateFormat.Hm().format(currentDate);
                     var Note_Model = note_Model(
                         title: title!,
                         subtitle: subtitle!,
-                        date: DateTime.now().toString());
+                        date: formateDate);
                     BlocProvider.of<addNotesCubit>(context).addNote(Note_Model);
                   } else {
                     autovalidateMode = AutovalidateMode.always;
